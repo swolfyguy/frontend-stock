914fbb1b60148173733f82dd64ea03c1d7547430
Mahesh Kokare <maheshkokare@Maheshs-MacBook-Air.local>
2022-01-14 02:48:55 +0530

    yesterday profit

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -17,18 +17,19 @@ super(props)
   
 }
 
-name(params) {
-  console.log("hello")
-}
+
 async componentDidMount() {
   try {
       const res = await fetch("https://flaskstockapi.herokuapp.com/api/profit");
+      const till_yesterday_profit = await fetch("https://flaskstockapi.herokuapp.com/api/till_yesterdays_profit?filter[date]=${yesterday}")
       const data = await res.json();
+      const profit = await till_yesterday_profit.json();
       this.setState({
         isLoaded:true,
-        items:data
+        items:data,
+        yesterday_profit: profit.data
       })
-      console.log("data printingn",data);
+      console.log("data printingn",profit.data);
   } catch(e) {
       console.error(e);
   }
@@ -36,9 +37,29 @@ async componentDidMount() {
 
 
   render() {
+    
     var {isLoaded } =  this.state;
+    // const sort_yesterday_profit= (till_yesterday) =>{
+    //   var d ={}
+    //   console.log("datt",till_yesterday)
+    //   till_yesterday.map(element => {
+    //     var id = element.attributes.strategy_id
+    //     d[id] = element.attributes.profit
+    //   });
+    //   return d
+    //   console.log("hello")
+    // }
     const data = this.state.items.data
     const meta = this.state.items.meta
+    const p_f = this.state.yesterday_profit
+    
+    var y_profit={}
+    if(p_f){
+      p_f.map(element => {
+        y_profit[element.attributes.strategy_id] = element.attributes.profit
+      });
+    }
+    console.log("datt",y_profit[1])
     var ruppe_number = ""
     const numberFormat=(value) => {
       ruppe_number =  new Intl.NumberFormat('en-IN', {
@@ -63,12 +84,13 @@ async componentDidMount() {
     else{
       
       const rowColor = (value) => {
-      if (parseInt(value) >= 0) {
+      if (parseInt(value) >= 0 || value == "buy") {
          return 'green';
       } else {
          return 'red';
       }
    }
+
       const display = data.map((d, key) => {
         return (
             
@@ -77,7 +99,10 @@ async componentDidMount() {
               <th>{d.name}</th>
                <td className={rowColor(d.completed.profit)} >{numberFormat(d.completed.profit)} </td>
                <td className={rowColor(d.on_going.profit)} >{numberFormat(d.on_going.profit)} / {d.on_going.trades}</td>
-               <td className={rowColor(d.total.profit)} >{numberFormat(d.total.profit)} </td>
+               <td className={rowColor(d.on_going.action)}>{d.on_going.action.toUpperCase()}</td>
+               
+               <td className={rowColor(d.total.profit - y_profit[d.id])}> {numberFormat(d.total.profit - (y_profit[d.id]=y_profit[d.id] || 0))}</td>
+               <td className={rowColor(d.total.profit)} >{numberFormat(d.total.profit)} / {d.total.trades}</td>
             </tr>
 
           );
@@ -96,6 +121,9 @@ async componentDidMount() {
                   <th >stock_name</th>
                   <th >completed_profit</th>
                   <th >on_going_profit</th>
+                  <th >action</th>
+                  
+                  <th >todays_profit</th>
                   <th >total_profit</th>
 
                 </tr>
@@ -107,6 +135,8 @@ async componentDidMount() {
                   <th >Total Profit: </th>
                   <th className={rowColor(meta.total_completed_profits)} >{numberFormat(meta.total_completed_profits)}</th>
                   <th className={rowColor(meta.total_ongoing_profits)} >{numberFormat(meta.total_ongoing_profits)}</th>
+                  <th ></th>
+                  <th ></th>
                   <th className={rowColor(meta.total_profits)} >{numberFormat(meta.total_profits)}</th>
 
                 </tr>
